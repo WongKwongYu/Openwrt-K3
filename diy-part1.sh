995e2e25938f61e5403ecbdaeaee312adc433f56
WongKwongYu <12872451+WongKwongYu@users.noreply.github.com>
2022-03-19 19:14:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git small8 https://github.com/Lenyu2020/small-package' >>feeds.conf.default
+#echo 'src-git small8 https://github.com/Lenyu2020/small-package' >>feeds.conf.default
 
 #Compile K3 Only
 sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
