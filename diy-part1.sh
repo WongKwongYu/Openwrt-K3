9001d5cb99968fd91eeb927cc386b9b9b7dc8a61
WongKwongYu <12872451+WongKwongYu@users.noreply.github.com>
2022-04-23 13:33:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall；luci' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git small8 https://github.com/Lenyu2020/small-package' >>feeds.conf.default
 
